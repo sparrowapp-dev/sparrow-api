c6d7bebec38a57d841df4e217cf800755d2dbada
Astitva877 <ankurpatle18@gmail.com>
2024-05-21 17:29:37 +0530

    fix: Remove unused files []

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,27 +1,37 @@
 import { Global, Module } from "@nestjs/common";
 import { MongoClient, Db } from "mongodb";
-import { ContextService } from "./services/context.service";
 import { ConfigService } from "@nestjs/config";
-import { WorkspaceModule } from "../workspace/workspace.module";
-import { ApiResponseService } from "./services/api-response.service";
-import { ParserService } from "./services/parser.service";
-import { LoggingExceptionsFilter } from "./exception/logging.exception-filter";
 import pino from "pino";
+
+// ---- Module
+import { WorkspaceModule } from "../workspace/workspace.module";
+
+// ---- Filter
+import { LoggingExceptionsFilter } from "./exception/logging.exception-filter";
+
+// ---- Services
 import { ProducerService } from "./services/kafka/producer.service";
 import { ConsumerService } from "./services/kafka/consumer.service";
-import { BlobStorageController } from "./controllers/blobStorage.controller";
 import { BlobStorageService } from "./services/blobStorage.service";
+import { ApiResponseService } from "./services/api-response.service";
+import { ParserService } from "./services/parser.service";
+import { ContextService } from "./services/context.service";
 
+/**
+ * Common Module provides global services and configurations used across the application.
+ * Includes database connection setup, logging configuration, and various utility services.
+ */
 @Global()
 @Module({
-  imports: [WorkspaceModule],
-  controllers: [BlobStorageController],
+  imports: [WorkspaceModule], // Import the Workspace Module
+  controllers: [],
   providers: [
     {
       provide: "DATABASE_CONNECTION",
       inject: [ConfigService],
       useFactory: async (configService: ConfigService): Promise<Db> => {
         try {
+          // Connect to MongoDB using the URL from ConfigService
           const client = await MongoClient.connect(configService.get("db.url"));
           return client.db("sparrow");
         } catch (e) {
@@ -33,10 +43,10 @@ import { BlobStorageService } from "./services/blobStorage.service";
       provide: "ErrorLogger",
       useValue: pino(
         {
-          level: pino.levels.labels["50"],
+          level: pino.levels.labels["50"], // Set the log level to "error"
         },
         pino.destination({
-          dest: "./logs/error.log",
+          dest: "./logs/error.log", // Specify the log file destination
           sync: true,
           append: true,
           mkdir: true,
