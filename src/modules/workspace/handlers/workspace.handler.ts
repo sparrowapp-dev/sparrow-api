8b2091695d2b1eeaef5ee1651992564276bc3069
Astitva Patle <ankurpatle18@gmail.com>
2024-10-08 18:57:31 +0530

    feat: added default environment creation []

--- src/modules/workspace/handlers/workspace.handler.ts
+++ src/modules/workspace/handlers/workspace.handler.ts
@@ -4,6 +4,9 @@ import { TOPIC } from "@src/modules/common/enum/topic.enum";
 import { ConsumerService } from "@src/modules/common/services/kafka/consumer.service";
 import { SUBSCRIPTION } from "@src/modules/common/enum/subscription.enum";
 import { ConfigService } from "@nestjs/config";
+import { CollectionService } from "../services/collection.service";
+import { EnvironmentService } from "../services/environment.service";
+import { EnvironmentType } from "@src/modules/common/models/environment.model";
 
 @Injectable()
 export class WorkspaceHandler implements OnModuleInit {
@@ -11,6 +14,8 @@ export class WorkspaceHandler implements OnModuleInit {
     private readonly workspaceService: WorkspaceService,
     private readonly consumerService: ConsumerService,
     private readonly configService: ConfigService,
+    private readonly collectionService: CollectionService,
+    private readonly environmentService: EnvironmentService,
   ) {}
 
   async onModuleInit() {
@@ -22,7 +27,36 @@ export class WorkspaceHandler implements OnModuleInit {
         setTimeout(async () => {
           const messageString = message.value.toString();
           const messageJson = JSON.parse(messageString);
-          await this.workspaceService.create(messageJson);
+          const workspace = await this.workspaceService.create(messageJson);
+          const sampleEnvironment = {
+            name: "Sample Environment",
+            workspaceId: workspace.insertedId.toString(),
+            variable: [
+              {
+                key: "DEV",
+                value: "",
+                checked: true,
+              },
+              {
+                key: "",
+                value: "",
+                checked: false,
+              },
+            ],
+          };
+          const environment = await this.environmentService.createEnvironment(
+            sampleEnvironment,
+            EnvironmentType.LOCAL,
+          );
+          await this.workspaceService.addEnvironmentInWorkSpace(
+            workspace.insertedId.toString(),
+            {
+              id: environment.insertedId,
+              name: sampleEnvironment.name,
+              type: EnvironmentType.LOCAL,
+            },
+          );
+          // await this.collectionService.createCollection();
         }, this.configService.get("app.kafkaHitTimeInterval"));
       },
       onError: async (error) => {
