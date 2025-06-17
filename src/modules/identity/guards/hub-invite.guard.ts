aee075cba2e1d158efac1631bc79e64a224b7154
aakash.reddy@techdome.net.in <aakash.reddy@techdome.net.in>
2025-06-17 20:13:29 +0530

    fix: added for ai

--- src/modules/identity/guards/hub-invite.guard.ts
+++ src/modules/identity/guards/hub-invite.guard.ts
@@ -32,7 +32,7 @@ export class HubInviteGuard implements CanActivate {
 
     const teamPlanId = userTeam?.plan.id;
     const planData = await this.planService.get(teamPlanId.toString());
-    if (teamUserEmails.size > planData?.limits?.usersPerHub?.value) {
+    if (teamUserEmails.size > planData?.limits?.usersPerHub?.value + 1) {
       throw new ForbiddenException("Plan limit reached");
     }
     return true;
