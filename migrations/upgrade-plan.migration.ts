38a296518cf239fffaaa3f913facd76409ac6e7e
Md Asif Raza <itsmdasifraza@gmail.com>
2025-06-26 05:07:11 -0700

    feat: create plan redundant to teams model [SPRW-1398]

--- migrations/upgrade-plan.migration.ts
+++ migrations/upgrade-plan.migration.ts
@@ -1,10 +1,6 @@
 import { Injectable, OnModuleInit, Inject } from "@nestjs/common";
 import { Collections } from "@src/modules/common/enum/database.collection.enum";
-import { Db, ObjectId } from "mongodb";
-
-const planId = "684"; 
-const planName = "Community"; 
-
+import { Db } from "mongodb";
 
 @Injectable()
 export class UpgradePlanMigration implements OnModuleInit {
@@ -23,28 +19,38 @@ export class UpgradePlanMigration implements OnModuleInit {
         `\n\x1b[32m[Nest]\x1b[0m \x1b[32mExecuting Upgrade plan Data Migration...`,
       );
       const teamsCollection = this.db.collection(Collections.TEAM);
+      const planCollection = this.db.collection(Collections.PLAN);
       const workspaceCollection = this.db.collection(Collections.WORKSPACE);
 
+      let teamResponseCount = 0;
+      const plans = await planCollection.find().toArray();
+      for(let i = 0; i < plans.length; i++){
+        let plan = plans[i];
+        plan.id = plan._id;
+        delete plan._id;
+        console.log(plan.name);
+        const teamResponse = await teamsCollection.updateMany(
+          { 'plan.name': plan.name }, 
+          {
+            $set: {
+              'plan': plan,
+            },
+          }
+        );
+        teamResponseCount += teamResponse?.matchedCount;
+      }
 
-      const teamResponse  = await teamsCollection.updateMany(
-        { }, // Replace with the actual team ID
-        { $set: {
-          'plan.id': new ObjectId(planId),
-          'plan.name': planName,
-        }, },
-      );
 
       const workspaceResponse = await workspaceCollection.updateMany(
         {},
         {
-          $set: {
-            'plan.id': new ObjectId(planId),
-            'plan.name': planName,
+          $unset: {
+            plan: "",
           },
         }
       );
       console.log(
-        `\x1b[32m[Nest] \x1b[33m${(teamResponse?.matchedCount + workspaceResponse?.matchedCount) || 0}\x1b[0m \x1b[32maffected documents, migration completed successfully.`,
+        `\x1b[32m[Nest] \x1b[33m${(teamResponseCount + workspaceResponse?.matchedCount) || 0}\x1b[0m \x1b[32maffected documents, migration completed successfully.`,
       );
       this.hasRun = true; // Set flag after successful execution
     } catch (error) {
