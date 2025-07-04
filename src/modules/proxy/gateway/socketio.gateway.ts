33e5004974929b6fd63efa00dc71866d70a32c1b
mithesh54 <mithesh.bangera@techdome.net.in>
2025-07-04 20:36:18 +0530

    fix: socket-path

--- src/modules/proxy/gateway/socketio.gateway.ts
+++ src/modules/proxy/gateway/socketio.gateway.ts
@@ -13,7 +13,7 @@ export const SOCKET_IO_PORT = 9001;
     origin: "*",
     methods: ["GET", "POST"],
   },
-  path: "/socket.io",
+  path: process.env.APP_ENV === "DEV" ? "/v2/socket.io" : "/socket.io",
   transports: ["websocket"],
 })
 export class SocketIoGateway implements OnGatewayConnection {
