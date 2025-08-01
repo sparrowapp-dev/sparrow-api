cb32d3263a717ed2e7df6c07f421b2b53587f33e
prayas17 <prayas1711@gmail.com>
2025-08-01 17:05:11 +0530

    fix: llm encryption

--- src/modules/workspace/services/collection-request.service.ts
+++ src/modules/workspace/services/collection-request.service.ts
@@ -39,6 +39,7 @@ import { ProducerService } from "@src/modules/common/services/event-producer.ser
 import { DecodedUserObject } from "@src/types/fastify";
 import { EncryptionService } from "@src/modules/common/services/encryption.service";
 import { Workspace } from "@src/modules/common/models/workspace.model";
+import { AiAssistantService } from "./ai-assistant.service";
 @Injectable()
 export class CollectionRequestService {
   constructor(
@@ -48,6 +49,7 @@ export class CollectionRequestService {
     private readonly branchRepository: BranchRepository,
     private readonly producerService: ProducerService,
     private readonly encryptionService: EncryptionService,
+    private readonly aiAssistantService: AiAssistantService
   ) {}
 
   async addFolder(
@@ -1618,7 +1620,26 @@ export class CollectionRequestService {
     };
     let updateMessage = ``;
     if (aiRequest.items.type === ItemTypeEnum.AI_REQUEST) {
-      aiRequestObj.aiRequest = aiRequest.items.aiRequest;
+
+      let encryptedAuthValue: string | undefined;
+      if (aiRequest.items.aiRequest?.auth?.apiKey?.authValue) {
+          encryptedAuthValue = this.encryptionService.encrypt(
+            aiRequest.items.aiRequest.auth.apiKey.authValue as string,
+          );
+          aiRequestObj.aiRequest = {
+          ...aiRequest.items.aiRequest,
+          auth: {
+            ...aiRequest.items.aiRequest.auth,
+            apiKey: {
+              ...aiRequest.items.aiRequest.auth.apiKey,
+              authValue: encryptedAuthValue,
+            },
+          },
+        };
+      }
+      else {
+        aiRequestObj.aiRequest = aiRequest.items.aiRequest;
+      }
 
       await this.collectionReposistory.addAiRequest(
         aiRequest.collectionId,
@@ -1643,8 +1664,58 @@ export class CollectionRequestService {
         }),
       });
 
-      return aiRequestObj;
+      if (aiRequest.items.aiRequest?.auth?.apiKey?.authValue) {
+          return {
+        ...aiRequestObj,
+        aiRequest: {
+          ...aiRequestObj.aiRequest,
+          auth: {
+            ...aiRequestObj.aiRequest.auth,
+            apiKey: {
+              ...aiRequestObj.aiRequest.auth.apiKey,
+              authValue: this.encryptionService.decrypt(
+                aiRequestObj.aiRequest.auth.apiKey.authValue as string,
+              ),
+            },
+          },
+        },
+      };
+      }
+      else {
+        return aiRequestObj;
+      }
+
+
     } else {
+      if (aiRequest.items.aiRequest?.auth?.apiKey?.authValue) {
+        const encryptedAuthValue = this.encryptionService.encrypt(
+        aiRequest.items.items.aiRequest.auth.apiKey.authValue as string,
+      );
+      aiRequestObj.items = [
+        {
+          id: uuidv4(),
+          name: aiRequest.items.items.name,
+          type: aiRequest.items.items.type,
+          description: aiRequest.items.items.description,
+          aiRequest: {
+            ...aiRequest.items.items.aiRequest,
+            auth: {
+              ...aiRequest.items.items.aiRequest.auth,
+              apiKey: {
+                ...aiRequest.items.items.aiRequest.auth.apiKey,
+                authValue: encryptedAuthValue,
+              },
+            },
+          },
+          source: SourceTypeEnum.USER,
+          createdBy: user?.name,
+          updatedBy: user?.name,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+    }
+    else {
       aiRequestObj.items = [
         {
           id: uuidv4(),
@@ -1659,6 +1730,7 @@ export class CollectionRequestService {
           updatedAt: new Date(),
         },
       ];
+    }
       await this.collectionReposistory.addAiRequestInFolder(
         aiRequest.collectionId,
         aiRequestObj,
@@ -1682,8 +1754,28 @@ export class CollectionRequestService {
           workspaceId: aiRequest.workspaceId,
         }),
       });
-
-      return aiRequestObj.items[0];
+      if (aiRequest.items.aiRequest?.auth?.apiKey?.authValue) {
+        // Decrypt before returning
+        const decryptedItem = {
+          ...aiRequestObj.items[0],
+          aiRequest: {
+            ...aiRequestObj.items[0].aiRequest,
+            auth: {
+              ...aiRequestObj.items[0].aiRequest.auth,
+              apiKey: {
+                ...aiRequestObj.items[0].aiRequest.auth.apiKey,
+                authValue: this.encryptionService.decrypt(
+                  aiRequestObj.items[0].aiRequest.auth.apiKey.authValue as string,
+                ),
+              },
+            },
+          },
+        };
+        return decryptedItem;
+      }
+      else {
+        return aiRequestObj.items[0];
+      }
     }
   }
 
@@ -1705,6 +1797,21 @@ export class CollectionRequestService {
       user._id,
     );
     await this.checkPermission(aiRequest.workspaceId, user._id);
+    // Encrypt only apiKey.authValue
+      const encryptedAuthValue = this.encryptionService.encrypt(
+        aiRequest.items.aiRequest.auth.apiKey.authValue as string,
+      );
+
+      aiRequest.items.aiRequest = {
+        ...aiRequest.items.aiRequest,
+        auth: {
+          ...aiRequest.items.aiRequest.auth,
+          apiKey: {
+            ...aiRequest.items.aiRequest.auth.apiKey,
+            authValue: encryptedAuthValue,
+          },
+        },
+      };
     const collection = await this.collectionReposistory.updateAiRequest(
       aiRequest.collectionId,
       aiRequestId,
