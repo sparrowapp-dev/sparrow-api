9751730ff28aab5087ad9fa5ee8c5fc3510a1c60
aakash.reddy@techdome.net.in <aakash.reddy@techdome.net.in>
2025-08-12 23:10:43 +0530

    feat: inserting generated variables into the collection items

--- src/modules/workspace/payloads/collectionRequest.payload.ts
+++ src/modules/workspace/payloads/collectionRequest.payload.ts
@@ -32,6 +32,7 @@ import {
   Auth,
   KeyValue,
 } from "@src/modules/common/models/collection.rxdb.model";
+import { VariableDto } from "@src/modules/common/models/environment.model";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
@@ -918,4 +919,31 @@ export class UpdateMockResponseRatioDto {
   @ValidateNested({ each: true })
   @Type(() => MockResponseRatioDto)
   mockResponseRatios: MockResponseRatioDto[];
+}
+
+export class CollectionGeneratedVariableDto {
+  @ApiProperty({ example: "6538e910aa77d958912371f5" })
+  @IsString()
+  @IsNotEmpty()
+  collectionId: string;
+
+  @ApiProperty({ example: "6538e910aa77d958912371f5" })
+  @IsString()
+  @IsNotEmpty()
+  workspaceId: string;
+
+  @ApiProperty({
+    required: true,
+    example: [
+      {
+        key: "key",
+        value: "value",
+        checked: true,
+      },
+    ],
+  })
+  @IsArray()
+  @Type(() => VariableDto)
+  @ValidateNested({ each: true })
+  generatedeVariables: VariableDto[];
 }
